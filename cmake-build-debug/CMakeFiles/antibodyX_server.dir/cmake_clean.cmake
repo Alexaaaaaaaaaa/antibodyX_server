5e02626a62b01bd2145c91d9434c2d89695cbc74
alex <1046212414@qq.com>
2023-03-11 02:00:07 +0800

    initial commit on 2023 3.11 01:59

---
+++ cmake-build-debug/CMakeFiles/antibodyX_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/antibodyX_server.dir/main.cpp.o"
+  "CMakeFiles/antibodyX_server.dir/main.cpp.o.d"
+  "antibodyX_server"
+  "antibodyX_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/antibodyX_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
